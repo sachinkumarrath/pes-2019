fd9165f1ab690a89d7c9f0a1bddc4b0c0fef908d
sachin kumar rath <sachinbubun@gmail.com>
2019-07-07 23:37:33 -0700

    port added

--- src/services/server.js
+++ src/services/server.js
@@ -4,7 +4,7 @@ import webpack from "webpack";
 import webpackDevMiddleware from "webpack-dev-middleware";
 import config from "../../webpack.config.dev";
 
-const port = 1900;
+const port = process.env.PORT || 1900;
 const app = express();
 const compiler = webpack(config);
 
